ab35a5e77495ff70d8948c8c7f63f42db8351e8a
Sujay Dana <sujay.dana@hotmail..com>
2024-03-02 11:19:55 +0100

    fixed main.tf with location

--- IaC-Terraform/main.tf
+++ IaC-Terraform/main.tf
@@ -56,6 +56,7 @@ resource "azurerm_sql_server" "sql_server" {
 resource "azurerm_sql_database" "sql_database" {
   name                = var.sql_database_name
   resource_group_name = var.resource_group_name
+  location            = var.location
   server_name         = azurerm_sql_server.sql_server.name
   edition             = "Standard"
   collation           = "SQL_Latin1_General_CP1_CI_AS"
@@ -66,5 +67,6 @@ resource "azurerm_sql_database" "sql_database" {
 resource "azurerm_application_insights" "app_insights" {
   name                = var.app_insights_name
   resource_group_name = var.resource_group_name
+  location            = var.location
   application_type    = "web"
 }
