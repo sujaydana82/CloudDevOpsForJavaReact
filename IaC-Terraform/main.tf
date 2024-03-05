3fdcaf53758068c30722fcd6a41c4fb0e07346fa
Sujay Dana <sujay.dana@hotmail..com>
2024-03-05 16:44:52 +0100

    modified tf files

--- IaC-Terraform/main.tf
+++ IaC-Terraform/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   name                = var.app_service_plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  kind                = "Windows"
+  kind                = "linux"
   sku {
     tier = "Basic"
     size = "B1"
--- IaC-Terraform/outputs.tf
+++ IaC-Terraform/outputs.tf
@@ -1,9 +1,4 @@
-output "storage_account_id" {
-  value = azurerm_storage_account.storage_account.id
-}
 
 output "app_service_url" {
   value = azurerm_app_service.app_service.default_site_hostname
 }
-
-# Add more outputs as needed for each environment
