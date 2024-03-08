0fa72ffc4d78b5b346495308586c56d68110d76b
Sujay Dana <sujay.dana@hotmail..com>
2024-03-08 17:44:21 +0100

    update files

--- IaC-Terraform/main.tf
+++ IaC-Terraform/main.tf
@@ -60,7 +60,7 @@ resource "azurerm_mssql_server" "sql_server" {
 resource "azurerm_sql_database" "sql_database" {
   name                = var.sql_database_name
   resource_group_name = var.resource_group_name
-  server_name         = azurerm_sql_server.sql_server.name
+  server_name         = azurerm_mssql_server.sql_server.name
   location            = var.location
   edition             = "Standard"
   collation           = "SQL_Latin1_General_CP1_CI_AS"
