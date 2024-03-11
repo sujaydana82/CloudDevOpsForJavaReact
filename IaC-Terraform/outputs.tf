f3e839711c1ffadc161719c6f22c9ef3c668d0ac
Sujay Dana <sujay.dana@hotmail..com>
2024-03-11 13:24:41 +0100

    adjusted some codes

--- IaC-Terraform/outputs.tf
+++ IaC-Terraform/outputs.tf
@@ -20,4 +20,5 @@ output "log_analytics_workspace_id" {
 
 output "application_insights_instrumentation_key" {
   value = azurerm_application_insights.app_insights.instrumentation_key
+  sensitive = true
 }
