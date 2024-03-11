376702fa2676e901922283deaeb42d940625cca6
Sujay Dana <sujay.dana@hotmail..com>
2024-03-11 23:41:58 +0100

    databaseloader.java updated

--- react-and-spring-data-rest/src/main/java/com/contoso/payroll/DatabaseLoader.java
+++ react-and-spring-data-rest/src/main/java/com/contoso/payroll/DatabaseLoader.java
@@ -2,9 +2,9 @@ package com.contoso.payroll;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.authority.AuthorityUtils;
+//import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
